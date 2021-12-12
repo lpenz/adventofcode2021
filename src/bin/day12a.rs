c5db2d06bd39fd3b84dcce52b3d189279b2918ca
Leandro Lisboa Penz <lpenz@lpenz.org>
2021-12-12 12:30:16 +0000

    Day 12a but "interning" cave names

--- src/bin/day12a.rs
+++ src/bin/day12a.rs
@@ -2,56 +2,83 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 
+use anyhow::anyhow;
 use anyhow::Result;
-use lazy_static::lazy_static;
 use std::collections::HashMap;
-// use std::collections::HashSet;
 use std::io::{stdin, BufRead};
 
 extern crate adventofcode2021;
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-pub struct Cave(String);
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub struct Cave(usize);
 
-impl Cave {
-    pub fn is_big(&self) -> bool {
-        if let Some(c) = self.0.chars().next() {
+#[derive(Debug, Default)]
+pub struct Caves {
+    pub names: Vec<String>,
+    pub ids: HashMap<String, usize>,
+    pub big: Vec<bool>,
+    pub map: HashMap<Cave, Vec<Cave>>,
+}
+
+impl Caves {
+    pub fn cave_get_or_insert(&mut self, cavename: String) -> Cave {
+        if let Some(&id) = self.ids.get(cavename.as_str()) {
+            return Cave(id);
+        }
+        let id = self.names.len();
+        self.big.push(if let Some(c) = cavename.chars().next() {
             c.is_uppercase()
         } else {
             false
-        }
+        });
+        self.names.push(cavename.clone());
+        self.ids.insert(cavename, id);
+        self.map.insert(Cave(id), vec![]);
+        Cave(id)
     }
-}
 
-impl From<&str> for Cave {
-    fn from(s: &str) -> Self {
-        Cave(s.to_string())
+    pub fn insert_edge(&mut self, edge: (String, String)) {
+        let (c1string, c2string) = edge;
+        let c1 = self.cave_get_or_insert(c1string);
+        let c2 = self.cave_get_or_insert(c2string);
+        let e = self.map.entry(c1).or_insert_with(Vec::new);
+        e.push(c2);
+        let e = self.map.entry(c2).or_insert_with(Vec::new);
+        e.push(c1);
     }
-}
 
-lazy_static! {
-    static ref START: Cave = Cave::from("start");
-    static ref END: Cave = Cave::from("end");
+    pub fn is_big(&self, cave: &Cave) -> bool {
+        self.big[cave.0]
+    }
+
+    pub fn cave_get(&self, cavename: impl AsRef<str>) -> Result<Cave> {
+        Ok(Cave(*self.ids.get(cavename.as_ref()).ok_or_else(|| {
+            anyhow!("could not find cave {}", cavename.as_ref())
+        })?))
+    }
+
+    pub fn iter_next(&self, cave: &Cave) -> impl Iterator<Item = &Cave> {
+        self.map[cave].iter()
+    }
 }
 
 // Parser
 
 pub mod parser {
-    use crate::Cave;
     use anyhow::anyhow;
     use anyhow::Result;
     use nom::{character, combinator, multi::*, IResult};
     use std::io::BufRead;
 
-    pub fn edge(input: &str) -> IResult<&str, (Cave, Cave)> {
+    pub fn edge(input: &str) -> IResult<&str, (String, String)> {
         let (input, cave1) = character::complete::alpha1(input)?;
         let (input, _) = character::complete::char('-')(input)?;
         let (input, cave2) = character::complete::alpha1(input)?;
         let (input, _) = character::complete::char('\n')(input)?;
-        Ok((input, (Cave::from(cave1), Cave::from(cave2))))
+        Ok((input, (cave1.to_string(), cave2.to_string())))
     }
 
-    pub fn parse(mut bufin: impl BufRead) -> Result<Vec<(Cave, Cave)>> {
+    pub fn parse(mut bufin: impl BufRead) -> Result<Vec<(String, String)>> {
         let mut input = String::default();
         bufin.read_to_string(&mut input)?;
         let result = combinator::all_consuming(many1(edge))(&input);
@@ -64,41 +91,32 @@ pub mod parser {
 
 // Main functions
 
-type Map = HashMap<Cave, Vec<Cave>>;
-
-fn dfs(map: &Map, current: &Cave, path: &mut Vec<Cave>) -> usize {
-    if *current == *END {
-        eprintln!("path: {:?}", path);
+fn dfs(caves: &Caves, current: &Cave, end: &Cave, path: &mut Vec<Cave>) -> usize {
+    if current == end {
         return 1;
     }
     let mut numpaths = 0;
-    if let Some(caves) = map.get(current) {
-        for cave in caves {
-            if !cave.is_big() && path.contains(cave) {
-                eprintln!("skip at path: {:?}, cave {}", path, cave.0);
-                continue;
-            }
-            eprintln!("{} -> {}", current.0, cave.0);
-            path.push(cave.clone());
-            numpaths += dfs(map, cave, path);
-            path.pop();
+    for cave in caves.iter_next(current) {
+        if !caves.is_big(cave) && path.contains(cave) {
+            continue;
         }
+        path.push(*cave);
+        numpaths += dfs(caves, cave, end, path);
+        path.pop();
     }
     numpaths
 }
 
 fn process(bufin: impl BufRead) -> Result<usize> {
     let edges = parser::parse(bufin)?;
-    let mut map = Map::new();
-    for (cave1, cave2) in edges {
-        let e = map.entry(cave1.clone()).or_insert_with(Vec::new);
-        e.push(cave2.clone());
-        let e = map.entry(cave2).or_insert_with(Vec::new);
-        e.push(cave1);
+    let mut caves = Caves::default();
+    for edge in edges.into_iter() {
+        caves.insert_edge(edge);
     }
-    eprintln!("{:?}", map);
-    let mut path = vec![START.clone()];
-    let paths = dfs(&map, &*START, &mut path);
+    let start = caves.cave_get("start")?;
+    let end = caves.cave_get("end")?;
+    let mut path = vec![start];
+    let paths = dfs(&caves, &start, &end, &mut path);
     Ok(paths)
 }
 
